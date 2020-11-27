ac302f6f6708db2fcb7b521a0f93c3bd9f625d1f
dashu202020 <72259716+dashu202020@users.noreply.github.com>
2020-11-27 23:55:56 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,5 @@
-import xlrd
-xls=xlrd.open_wordbook("C:\Users\Administrator\Desktop\测试2.xlsx")
+# import xlrd
+# xls=xlrd.open_wordbook("C:\Users\Administrator\Desktop\测试2.xlsx")
+
+
+print('这是python 的测试')
